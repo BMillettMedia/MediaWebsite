3e2f76d30d64c1bc8deeb81cf1ca70485f0978b4
bmillettmedia <bryanmillett@cox.net>
2025-05-08 14:46:37 -0700

    styling update: Portfolio slideshow corrections

---
+++ script/BM_nav.js
@@ -0,0 +1,21 @@
+// nav.js
+// Dynamically inserts navigation menu
+document.addEventListener("DOMContentLoaded", () => {
+    const header = document.querySelector("header");
+    if (header) {
+        header.innerHTML = `
+            <img src="images/BM_Images/BMillettMedia_transparent.png" alt="Site Logo">
+            <nav class="BM_nav">
+                <ul>
+                    <li><a href="bm-home.html">Home</a></li>
+                    <li><a href="bm-about.html">About Me</a></li>
+                    <li><a href="bm-resume.html">Resume</a></li>
+                    <li><a href="bm-portfolio.html">Portfolio</a></li>
+                    <li><a href="mw-home.html">Mentally_Wrong</a></li>
+                </ul>
+            </nav>
+        `;
+    } else {
+        console.warn("No <header> found for navigation.");
+    }
+});
