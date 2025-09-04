c66be0d5a145af14027657b6051df69a0decb109
bmillettmedia <bryanmillett@cox.net>
2025-09-04 12:48:17 -0700

    nav update

--- script/BM_nav.js
+++ script/BM_nav.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // Header navigation
     if (header) {
         header.innerHTML = `
-            <img src="images/BM_Images/BMillettMedia_transparent.png" alt="Site Logo" class="logo">
+            <a href="bm-home.html"> <img src="images/BM_Images/BMillettMedia_transparent.png" alt="Site Logo" class="logo"> </a>
             <nav class="nav">
                 <ul>
                     <li><a href="bm-home.html">Home</a></li>
